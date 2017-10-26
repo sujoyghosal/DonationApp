c3b2f03a4533ed25e357696f09370e670e1af720
sujoyghosal <sujoy.ghosal@gmail.com>
2017-10-26 14:13:36 +0530

    Folder Cleanup

--- deploy/apigeenpm.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Authorization: Basic Z3N1am95QHlhaG9vLmNvbTpLb2xrYXRhNDE=" -d "command=install" "https://api.enterprise.apigee.com/v1/organizations/sujoyghosal/apis/donationsapi/revisions/1/npm"
