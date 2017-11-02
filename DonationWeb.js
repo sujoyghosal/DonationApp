e3c3c0f3ea49850d4428970ab1fa88d4f9112af9
sujoyghosal <sujoy.ghosal@gmail.com>
2017-11-02 17:40:56 +0530

    Cleanup

--- DonationWeb.js
+++ DonationWeb.js
@@ -23,7 +23,7 @@ app.get('/offeredrides', function(req, res) {
 app.get('/myoffers', function(req, res) {
     res.sendFile(path.join(__dirname + '/www/MyOffers.html'));
 })
-var port = process.env.PORT || 9500;
+var port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
--- access_apigee.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST "https://apibaas-trial.apigee.net/sujoyghosal/freecycle/token" -d '{"grant_type":"client_credentials", "client_id":"b3U6qZdN9MaZEeanNBIuBzeXfQ", "client_secret":"b3U6-cw_nkkX9VRDgrvKwi0ofs2Sr4E"}'
