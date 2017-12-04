f056ad7fe3d95114505b4d4c4535384943cde5ad
sujoyghosal <sujoy.ghosal@gmail.com>
2017-12-04 16:45:03 +0530

    Single Bluemix backend

--- www/js/myappangular.js
+++ www/js/myappangular.js
@@ -243,12 +243,11 @@ app.service("UserService", function() {
     };
 });
 
-var BASEURL_DEV = "https://freecycleapissujoy.mybluemix.net";
-var BASEURL_PROD = "https://sujoyfreecycleeventsapi.mybluemix.net";
+var BASEURL_BLUEMIX = "https://freecycleapissujoy.mybluemix.net";
 var BASEURL_LOCAL = "http://localhost:9000";
 var BASEURL_PIVOTAL = "http://freecycleapissujoy-horned-erasure.cfapps.io";
 
-var BASEURL = BASEURL_PROD;
+var BASEURL = BASEURL_BLUEMIX;
 
 var GEOCODEURL = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA_sdHo_cdsKULJF-upFVP26L7zs58_Zfg";
 
